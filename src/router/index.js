e04ab5580f9739b8bd427181ca54b8bc9bdc9748
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-12-07 23:32:49 +0800

    Updated main router to fix broken home route:     Updated router/index.js to fix the broken route caused by changing     Home.vue to Timers.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Timers from "../views/Timers.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: Home
+    name: "timers",
+    component: Timers
   },
   {
     path: "/about",
