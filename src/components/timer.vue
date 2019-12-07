072c90d76e9b61a76d94b5ddac0b7faabcaf872d
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-12-07 20:53:00 +0800

    Created timer and removed HelloWorld components: 1.  Created timer component, which will be the view component for     individual timers with basic scaffolded code. 2.  Deleted HelloWorld component.

---
+++ src/components/timer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="timer">
+  </div>
+</template>
+
+<script>
+export default {
+	name: "Timer",
+	// Might want to allow props to be passed in to create timers, else, perhaps there will also be default values
+	props: {
+		name: String,
+		description: String,
+		time: Number
+	}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
