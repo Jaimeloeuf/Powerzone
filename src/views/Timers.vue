2a567dc138445f3d3992645fd02bdbbca6e5a278
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-12-07 23:21:46 +0800

    Modified Home view into Timers view:     Created basic timers view, which will be the first view users' see,     with all the timers after they login, using Home view as scaffold.

---
+++ src/views/Timers.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+
+    <ul>
+      <li v-for="timer in timers" v-bind:key="timer.id">
+        <Timer v-bind:timer="timer"/>
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+import Timer from "@/components/timer.vue";
+
+// Get all timers from vuex store, then pass them into individual time components
+
+export default {
+  name: "home",
+  components: {
+    Timer
+  },
+  data: {
+    timers: [
+      { id: 1 },
+      { id: 2 },
+      { id: 3 }
+    ]
+  }
+};
+</script>
