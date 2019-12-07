4cc2976e59af7caa2a0a4f611358967c52ae2de1
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-12-07 20:54:36 +0800

    Updated eslintrc config and ran eslint fix: 1.  Updated eslintrc for a few rule changes. 2.  Ran eslint --fix on all .js and .vue files in the repo.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,27 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
-}
+	"root": true,
+	"env": {
+		"es6": true,
+		"node": true
+	},
+	"extends": [
+		"plugin:vue/essential",
+		"@vue/standard"
+	],
+	"rules": {
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"always"
+		],
+		"camelcase": [
+			"error"
+		]
+	},
+	"parserOptions": {
+		parser: "babel-eslint"
+	}
+};
